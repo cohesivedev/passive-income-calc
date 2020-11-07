0808f77c81d23c488465b43b34b1e981c3931b73
jsyang <jsyang.ca@gmail.com>
2020-11-07 01:44:57 +0000

    search working

--- getRankings.js
+++ getRankings.js
@@ -1,5 +1,6 @@
-const fetch   = require('node-fetch');
-const {parse} = require('node-html-parser');
+const fetch           = require('node-fetch');
+const {parse}         = require('node-html-parser');
+const {writeFileSync} = require('fs');
 
 fetch('https://www.fundsexplorer.com.br/ranking')
     .then(res => res.text())
@@ -12,6 +13,19 @@ fetch('https://www.fundsexplorer.com.br/ranking')
             indexTableRanking, indexTableRankingEnd
         ));
 
-        console.log(root.querySelectorAll('tbody'));
+        let outputJSON = {};
+
+        const rowHeaders = root.querySelectorAll('thead tr');
+        for (let r of rowHeaders) {
+            outputJSON.headers = r.querySelectorAll('th').map(th => th.rawText);
+        }
+
+        outputJSON.rankings = [];
+        const rows = root.querySelectorAll('tbody tr');
+        for (let r of rows) {
+            outputJSON.rankings.push(r.querySelectorAll('td').map(td => td.rawText));
+        }
+
+        writeFileSync('rankings.json', JSON.stringify(outputJSON));
     });
 
--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-fetch('https://www.fundsexplorer.com.br/ranking', {
-    mode: 'no-cors',
-})
-.then(res=>res.text())
-.then(res=>console.log(res));
+import {rankings} from './rankings.json';
+
+const FILTER_NONE = () => true;
+
+const SORT_CODE_ALPHA = (r1, r2) => {
+    return r1[0].localeCompare(r2[0]);
+};
+
+function renderTable(filterFunc = FILTER_NONE) {
+    const sortedAndFiltered = rankings.filter(filterFunc);
+
+    sortedAndFiltered.sort(SORT_CODE_ALPHA);
+
+    document.getElementById('rankings').innerHTML = sortedAndFiltered
+        .map(
+            r => `<tr><td>${r[0]}</td><td>${r[2]}</td><td>${r[4]}</td><td>${r[5]}</td><td class="more"></td></tr>`
+        )
+        .join('');
+}
+
+function updateTableBySearch() {
+    const query = document.getElementById('search').value.trim().toUpperCase();
+
+    const startsWith = new RegExp('^' + query);
+    renderTable(r => startsWith.test(r[0]));
+}
+
+const el = {
+    search: document.getElementById('search')
+};
+
+el.search.addEventListener('keyup', updateTableBySearch);
+el.search.addEventListener('change', updateTableBySearch);
+
+renderTable();
